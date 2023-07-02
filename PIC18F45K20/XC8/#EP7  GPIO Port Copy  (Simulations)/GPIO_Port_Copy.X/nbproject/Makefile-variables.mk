4a455b8ad8ca97528df238df7f9b33c2d214317c
EEPUXProjects <50055478+EEPUXProjects@users.noreply.github.com>
2023-07-02 16:34:32 +0100

    Add files via upload

---
+++ PIC18F45K20/XC8/#EP7  GPIO Port Copy  (Simulations)/GPIO_Port_Copy.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=GPIO_Port_Copy.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/GPIO_Port_Copy.X.production.hex
