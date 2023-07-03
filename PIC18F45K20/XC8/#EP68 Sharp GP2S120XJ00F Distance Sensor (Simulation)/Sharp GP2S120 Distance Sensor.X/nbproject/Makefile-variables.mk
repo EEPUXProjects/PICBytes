41a610592d29ac54d379300176c5b85beb0fb8ec
EEPUXProjects <50055478+EEPUXProjects@users.noreply.github.com>
2023-07-03 21:53:54 +0100

    Add files via upload

---
+++ PIC18F45K20/XC8/#EP68 Sharp GP2S120XJ00F Distance Sensor (Simulation)/Sharp GP2S120 Distance Sensor.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Sharp_GP2S120_Distance_Sensor.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Sharp_GP2S120_Distance_Sensor.X.production.hex
