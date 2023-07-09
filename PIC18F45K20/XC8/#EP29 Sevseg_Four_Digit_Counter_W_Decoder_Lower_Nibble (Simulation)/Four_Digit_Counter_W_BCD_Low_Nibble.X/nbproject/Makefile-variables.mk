587c86d2b6680e62d314ecd109dc16d8ec2bbfbe
EEPUXProjects <50055478+EEPUXProjects@users.noreply.github.com>
2023-07-09 17:19:35 +0100

    Add files via upload

---
+++ PIC18F45K20/XC8/#EP29 Sevseg_Four_Digit_Counter_W_Decoder_Lower_Nibble (Simulation)/Four_Digit_Counter_W_BCD_Low_Nibble.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Four_Digit_Counter_W_BCD_Low_Nibble.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Four_Digit_Counter_W_BCD_Low_Nibble.X.production.hex
