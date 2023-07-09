941eed7c69dead4dd93f5748083ae0d76836e21f
EEPUXProjects <50055478+EEPUXProjects@users.noreply.github.com>
2023-07-09 09:48:24 +0100

    Add files via upload

---
+++ PIC18F45K20/XC8/#EP65 TMP36 Temperature Sensor Part 1 (Simulation)/TMP36_Temperature_Example_Part_1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TMP36_Temperature_Example_Part_1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TMP36_Temperature_Example_Part_1.X.production.hex
