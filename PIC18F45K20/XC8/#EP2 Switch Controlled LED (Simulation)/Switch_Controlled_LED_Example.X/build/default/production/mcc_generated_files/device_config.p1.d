4e675e9bb08890b678332892fbbc83d1d90dd789
EEPUXProjects <50055478+EEPUXProjects@users.noreply.github.com>
2023-07-28 23:41:14 +0100

    Add files via upload

---
+++ PIC18F45K20/XC8/#EP2 Switch Controlled LED (Simulation)/Switch_Controlled_LED_Example.X/build/default/production/mcc_generated_files/device_config.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/mcc_generated_files/device_config.d  \
+ build/default/production/mcc_generated_files/device_config.p1:  \
+ mcc_generated_files/device_config.c  \
+C:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-K_DFP/1.7.134/xc8/pic/include/language_support.h 
