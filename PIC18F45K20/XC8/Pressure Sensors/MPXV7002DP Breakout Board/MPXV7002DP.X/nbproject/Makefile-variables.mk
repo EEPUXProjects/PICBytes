e0ee90a7e7692bd6c9c712c482151b1fa6bd5663
EEPUXProjects <50055478+EEPUXProjects@users.noreply.github.com>
2023-11-25 22:02:37 +0000

    Add files via upload

---
+++ PIC18F45K20/XC8/Pressure Sensors/MPXV7002DP Breakout Board/MPXV7002DP.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MPXV7002DP.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MPXV7002DP.X.production.hex
